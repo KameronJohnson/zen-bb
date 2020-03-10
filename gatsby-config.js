28da4e2af0faa92d2be04510d5e8be29ec0c7f59
Kameron Johnson <kjohnson@parthenonsoftware.com>
2020-03-10 13:36:09 -0700

    add navbar with mobile toggling functionality, basic css

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,17 @@ module.exports = {
         icon: `src/images/icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      options: {
+        fonts: [
+          {
+            family: `Open Sans Condensed`,
+            variants: [`200`, `400`, `700`]
+          },
+        ],
+      },
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,18 @@
 import React from "react"
 import { Link } from "gatsby"
 
-import { FaCartPlus } from "react-icons/fa"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+
+import zenFrog from '../images/zen-frog.png'
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <FaCartPlus />
+    <h1 className="tagline">Zen Bodybuilding</h1>
+    <div className="d-flex justify-content-center">
+      <img src={zenFrog} className="zen-frog"></img>
+    </div>
   </Layout>
 )
 
